ebf4a36da24450686c041b461a87e070d84f94d9
Neli Kostadinova <nkostadinova@hhogdev.com>
2019-12-02 15:25:08 +0200

    Updates

---
+++ precache-manifest.a5dab7b01fe7a4bdb154c4ca1ddaeb9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b73b0dcf17c7c4b6b0d8b3aab3baf1",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "c4f06f3db8b3d2475326",
+    "url": "/memory-game/static/css/main.156a2bee.chunk.css"
+  },
+  {
+    "revision": "b462c2efc67e3d339101",
+    "url": "/memory-game/static/js/2.e45cb95c.chunk.js"
+  },
+  {
+    "revision": "c4f06f3db8b3d2475326",
+    "url": "/memory-game/static/js/main.5075c67d.chunk.js"
+  },
+  {
+    "revision": "3c6c1d3098f1f9b94d8d",
+    "url": "/memory-game/static/js/runtime-main.53e70fbf.js"
+  }
+]);
